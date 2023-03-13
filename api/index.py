6da2868cc739ff901ec30352fa3a8672b85cb535
josecc92 <51939862+josecc92@users.noreply.github.com>
2023-03-13 11:33:47 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -11,20 +11,23 @@ app.template_folder = TEMPLATE_DIR
 
 @app.before_first_request
 def create_database():
-    # 檢查資料庫檔案是否已存在
-    if not os.path.exists(f'{SERVER_DIR}/data/database.db'):
-        # 建立資料庫
-        conn = sqlite3.connect(f'{SERVER_DIR}/data/database.db')
-        # 在資料庫中創建表格
-        c = conn.cursor()
-        c.execute('''CREATE TABLE users
-                     (id INTEGER PRIMARY KEY AUTOINCREMENT,
-                      name TEXT NOT NULL,
-                      email TEXT NOT NULL,
-                      age INTEGER NOT NULL)''')
-        # 關閉資料庫連接
-        conn.close()
-
+    try:
+        # 檢查資料庫檔案是否已存在
+        if not os.path.exists(f'{SERVER_DIR}/data/database.db'):
+            # 建立資料庫
+            conn = sqlite3.connect(f'{SERVER_DIR}/data/database.db')
+            # 在資料庫中創建表格
+            c = conn.cursor()
+            c.execute('''CREATE TABLE users
+                         (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                          name TEXT NOT NULL,
+                          email TEXT NOT NULL,
+                          age INTEGER NOT NULL)''')
+            # 關閉資料庫連接
+            conn.close()
+    except Exception as e:
+        return f"發生錯誤: {str(e)}"
+    return 
 
 # domain root
 @app.route('/')
